4aecb60b95315410b57f289e89905cb325b8ee5b
aries <barlasss@gmail.com>
2024-10-15 18:37:17 +0300

    fix: target deleted.

--- shared/target/debug/build/proc-macro2-30968231008c2290/out/proc_macro2.d
+++
@@ -1,7 +0,0 @@
-/Users/aries/work/kutuama/inner-shelter/shared/target/debug/build/proc-macro2-30968231008c2290/out/libproc_macro2.rmeta: build/probe.rs
-
-/Users/aries/work/kutuama/inner-shelter/shared/target/debug/build/proc-macro2-30968231008c2290/out/proc_macro2.d: build/probe.rs
-
-build/probe.rs:
-
-# env-dep:RUSTC_BOOTSTRAP
--- shared/target/debug/build/serde-b7b90b70ff12751b/build_script_build-b7b90b70ff12751b.d
+++
@@ -1,5 +0,0 @@
-/Users/aries/work/kutuama/inner-shelter/shared/target/debug/build/serde-b7b90b70ff12751b/build_script_build-b7b90b70ff12751b: /Users/aries/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.210/build.rs
-
-/Users/aries/work/kutuama/inner-shelter/shared/target/debug/build/serde-b7b90b70ff12751b/build_script_build-b7b90b70ff12751b.d: /Users/aries/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.210/build.rs
-
-/Users/aries/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.210/build.rs:
