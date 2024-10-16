ffc18a89d41d511a37509a47204f265217ed205c
aries <barlasss@gmail.com>
2024-10-16 19:20:36 +0300

    fix: remove the warnings.

--- client/src/domain/models.rs
+++ client/src/domain/models.rs
@@ -3,12 +3,3 @@ pub struct User {
     pub username: String,
     pub token: Option<String>,
 }
-
-impl User {
-    pub fn new(username: String) -> Self {
-        Self {
-            username,
-            token: None,
-        }
-    }
-}
--- client/src/presentation/home_page.rs
+++ client/src/presentation/home_page.rs
@@ -3,11 +3,25 @@ use crate::presentation::login::LoginPage;
 use crate::presentation::register::RegisterPage;
 use crate::application::auth_service::AuthService;
 use crate::domain::models::User;
+use web_sys::console;
 
 #[component]
 pub fn HomePage(auth_service: AuthService) -> impl IntoView {
     let active_tab = create_rw_signal("login".to_string());
     let user = create_rw_signal(None::<User>);
+
+    // Function to log user data to the console
+    let log_user = move || {
+        if let Some(user) = user.get() {
+            let user_info = format!("Username: {}, Token: {:?}", user.username, user.token);
+            console::log_1(&user_info.into());
+        }
+    };
+
+    // Call log_user whenever the user signal is updated
+    create_effect(move |_| {
+        log_user();
+    });
 
     let select_login = move |_| active_tab.set("login".to_string());
     let select_create_account = move |_| active_tab.set("create".to_string());
